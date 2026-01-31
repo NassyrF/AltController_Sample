63d19337996d0bb868f0956917377eb5c399af4f
NassyrF <iredafat@pratt.edu>
2026-01-30 23:58:57 -0500

    Morse Code minigame works completely on the algorithmical side !! :)
    ya basically its gonna show u a morse and if u do the sequence correctly u get a "yayyy" but if u mess up it goes naurrr. and then it waits 2secs and gives you a new morse code to do!!! awesomesauce :)

--- Assets/Scripts/Morse_Mechanic.cs
+++ Assets/Scripts/Morse_Mechanic.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 using UnityEngine.Events;
 using System.Collections.Generic;
+using System.Collections;
 
 public class Morse_Mechanic : MonoBehaviour
 {
     public UnityEvent<string> changeDisplay;
     public int lengthOfMorse;
+    private string morseSentence;
     private List<string> code;
     private List<string> options;
+    private List<string> playerMorse;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print(generateMorse(3));
+        DisplayNewCode();
     }
 
     // Update is called once per frame
@@ -21,13 +24,9 @@ public class Morse_Mechanic : MonoBehaviour
         
     }
 
-    public void DisplayNewCode(){
-        changeDisplay.Invoke(generateMorse(lengthOfMorse));
-    }
-
- 
+    //Generates a random Morse string of length s_length
     private string generateMorse(int s_length){
-
+        playerMorse = new List<string>();
         code = new List<string>();
         options = new List<string>{"_", "."};
 
@@ -36,6 +35,49 @@ public class Morse_Mechanic : MonoBehaviour
         }
 
         return string.Join(" ",code);
-
     }
+
+
+    //Changes display in the UI to the new Morse Phrase to match
+    public void DisplayNewCode(){
+        morseSentence=generateMorse(lengthOfMorse);
+        changeDisplay.Invoke(morseSentence);
+    }
+
+
+    //Adds the player's input to a list, and if it's the same length as the sample,
+    //then it compares the vale=ues using the void under this one (CompareInput)
+    public void ReceivePlayerLetter(string letter){
+
+        playerMorse.Add(letter);
+        // print("INPUT SO FAR :"+string.Join(" ",playerMorse));
+        // print("playerLen: "+playerMorse.Count.ToString());
+        // print("puterLen: "+morseSentence.Length.ToString());
+
+        if(playerMorse.Count==lengthOfMorse){
+            CompareInput(string.Join(" ",playerMorse));
+        }
+    }
+
+
+    //Compares the values inputted by the player to the Computer's Morse string
+    public void CompareInput(string playerSentence){
+        if(playerSentence==morseSentence){
+            print("YAYYYY");
+        }
+        else{
+            print("NAURRRRR");
+        }
+        changeDisplay.Invoke(" ");
+        StartCoroutine("NewMorseDelay");
+    }
+
+
+    //After two seconds, a new code appears
+    private IEnumerator NewMorseDelay(){
+        yield return new WaitForSeconds(2f);
+        DisplayNewCode();
+    }
+
+ 
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class PlayerInput : MonoBehaviour
 {
     private bool keyDown;
     private float keyPressTime;
     public float desiredTime;
+    //private List<string> playerPhrase;
+    public UnityEvent<string> playerLetter;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         keyPressTime=0;
+        //playerPhrase = new List<string>();
     }
 
     // Update is called once per frame
@@ -24,11 +31,27 @@ public class PlayerInput : MonoBehaviour
     public void OnInteract(){
         if(keyDown){
             keyDown=false;
-            Debug.Log(keyPressTime);
-            keyPressTime=0;
+            constructSentence();
         }
         else{
             keyDown=true;
         }
+    }
+
+    private void constructSentence(){
+
+        Debug.Log(keyPressTime);
+
+            if(keyPressTime>=desiredTime){
+                //playerPhrase.Add("_");
+                playerLetter.Invoke("_");
+            }
+            else{
+                //playerPhrase.Add(".");
+                playerLetter.Invoke(".");
+            }
+
+            keyPressTime=0;
+
     }
 }
